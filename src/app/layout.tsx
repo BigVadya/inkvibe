0c6744bf3b54fd81f5d1b1db1f680d718ea0b2e7
BigV <14.88.berserk.i@gmail.com>
2025-06-04 17:32:27 +0300

    FUCK

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,35 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+import './globals.css'
 
 const geistSans = localFont({
-	src: "./fonts/GeistVF.woff",
-	variable: "--font-geist-sans",
-	weight: "100 900",
-});
+	src: './fonts/GeistVF.woff',
+	variable: '--font-geist-sans',
+	weight: '100 900',
+})
 const geistMono = localFont({
-	src: "./fonts/GeistMonoVF.woff",
-	variable: "--font-geist-mono",
-	weight: "100 900",
-});
+	src: './fonts/GeistMonoVF.woff',
+	variable: '--font-geist-mono',
+	weight: '100 900',
+})
 
 export const metadata: Metadata = {
-	title: "ACME - Innovative Solutions",
-	description: "ACME provides cutting-edge solutions for modern businesses",
-};
+	title: 'InkVibe',
+	description: 'InkVibe',
+}
 
 export default function RootLayout({
 	children,
 }: Readonly<{
-	children: React.ReactNode;
+	children: React.ReactNode
 }>) {
 	return (
-		<html lang="en" className="scroll-smooth">
+		<html lang='en' className='scroll-smooth'>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased bg-gray-50 text-gray-900 overflow-y-scroll`}
 			>
 				{children}
 			</body>
 		</html>
-	);
+	)
 }
